75da2c099964b812230cb0f45ac8456b0d45b8bb
Samson  Mamushet <forsamisams@gmail.com>
2025-06-15 12:08:32 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App: React.FC = () => {
   });
 //const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' });
 //const httpLink = new HttpLink({ uri: 'https://test.nilesoftdemo.com/graphql' });
-  const httpLink = new HttpLink({ uri: 'https://api.etproforma.com/graphql' });
+  const httpLink = new HttpLink({ uri: 'https://etpro.samsonmamushet.com/graphql' });
   const link = authLink.concat(httpLink);
   const client = new ApolloClient({
     link: link,
